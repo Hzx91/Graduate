a365dfaf8dd0a9789f573d52862d7dfb56d9c4fe
黄梓星 <3274801970@qq.com>
2026-03-26 22:41:49 +0800

    资源管理

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8082',
-        changeOrigin: true,
-        rewrite: (path) => path
-      }
-    }
-  }
+  plugins: [vue()]
 })
